b1cfdea91ae0bf9a6ba0f93164fd2e828f6521fd
VittorioValent <v.valent@contrader.it>
2021-08-24 00:02:24 +0200

    corretto persistable

--- src/main/java/it/jrolamo/generics/mongodb/domain/AbstractModel.java
+++ src/main/java/it/jrolamo/generics/mongodb/domain/AbstractModel.java
@@ -1,17 +1,18 @@
 package it.jrolamo.generics.mongodb.domain;
 
 import java.io.Serializable;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.Persistable;
 
 /**
  *
  * @author Vittorio Valent
  * @since 0.0.1
  */
-public abstract class AbstractModel implements Serializable {
+public abstract class AbstractModel implements Serializable, Persistable<String> {
 
-    /**
-     *
-     * @return
-     */
-    public abstract String getId();
+    @Override
+    public boolean isNew() {
+        return StringUtils.isBlank(getId());
+    }
 }
